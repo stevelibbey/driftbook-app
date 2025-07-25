b1f7539e0453e2322583892d0573c53669934072
Steve Libbey <stevelibbey@gmail.com>
2025-07-24 19:33:50 -0700

    Set base path for GitHub Pages

--- Driftbook/vite.config.js
+++ Driftbook/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite'
+
 export default defineConfig({
-  base: '/driftbook-app/', // or your repo name
-});
+  plugins: [react()],
+})
